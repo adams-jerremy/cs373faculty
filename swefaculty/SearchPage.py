70e5e5583a15b914a27e5bdf1261b80c44469c2b
beatle.eater <beatle.eater@d6fc1dee-b459-11de-b71e-ab0a53e69959>
2009-11-29 22:26:34 +0000

    updat

--- swefaculty/SearchPage.py
+++ swefaculty/SearchPage.py
@@ -15,277 +15,48 @@ import cgi
 
 def concat(x,y): return x+y;
 def xmap(uf,i): return (uf(v) for v in i);
-def mapReduce(bf,uf,i): return reduce(bf,xmap(uf,i));
+def mapReduce(bf,uf,i,d=''): return reduce(bf,xmap(uf,i),d);
 
 def select(name,table,col):
-    return '<select style="margin: 0.2em" multiple="multiple" size="10" name="'+name+'"><optgroup label="'+name+'">'+mapReduce(concat,lambda e:'<option value='+str(e.key())+'>'+e.__dict__['_'+col]+'</option>',table.all())+'</optgroup></select>'
+    return '<select style="margin: 0.2em" multiple="multiple" size="10" name="'+name+'"><optgroup label="'+name+'">'+mapReduce(concat,lambda e:'<option value='+str(e.key())+'>'+(''if e.__dict__['_'+col] is None else str(e.__dict__['_'+col]))+'</option>',table.all())+'</optgroup></select>'
     
-
+searches =[("Names",Datastore.Faculty,'name'),
+        ("Rooms",Datastore.Faculty,'room'),
+        ("Buildings",Datastore.building,'building'),
+        ("Days",Datastore.OfficeHourJoin,'day'),
+        ("Faculty Type",Datastore.faculty_type, 'faculty_type'),
+        ("Degree Type",Datastore.degree_type, 'degree_type'),
+        ("Degree Names",Datastore.degree_name, 'degree_name'),
+        ("Institutions",Datastore.institution, 'institution'),
+        ("Reasearch Areas", Datastore.research_area, 'research_area'),
+        ("Conference Names",Datastore.conference_name, 'conference_name'),
+        ("Conference Titles",Datastore.ConferenceJoin, 'title'),
+        ("Conference Locations",Datastore.location, 'location'),
+        ("Conference Years",Datastore.ConferenceJoin, 'year'),
+        ("Journals",Datastore.journal_name, 'journal_name'),
+        ("Books",Datastore.BookJoin, 'title'),
+        ("Publishers",Datastore.publisher, 'publisher'),
+        ("Graduate Student Types",Datastore.student_type, 'student_type'),
+        ("Course Numbers",Datastore.course_number, 'course_number'),
+        ("Course Types",Datastore.course_type, 'course_type'),
+        ("Semesters",Datastore.semester, 'semester'),
+        ("Award Types",Datastore.award_type, 'award_type'),
+        ("Award Titles",Datastore.AwardJoin, 'title'),
+        ("Award Years",Datastore.AwardJoin, 'year')]
+        
 class MainPage (webapp.RequestHandler) :
     def get(self) :
         self.response.out.write('<form id="SearchForm" method="post" action="/search">')
-        self.response.out.write(select("Names",Datastore.Faculty,'name'))
-        self.response.out.write(select("Rooms",Datastore.Faculty,'room'))
-        self.response.out.write(select("Buildings",Datastore.building,'building'))
-        self.response.out.write(select("Days",Datastore.OfficeHourJoin,'day'))
-        types = Datastore.Faculty.all()
-        """
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="name">')
+        for s in searches:
+            self.response.out.write(select(*s))
         
-        self.response.out.write('<optgroup label="Names">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(fac.key()) + '>')
-            self.response.out.write(fac.name)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        """
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="building">')
-        build = Datastore.building.all()
-        self.response.out.write('<optgroup label="Buildings">')
-        i = 1
-        for abr in build :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(abr.building)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
+     #  self.response.out.write(select("Graduate Students' EIDs",Datastore.student_type, 'student'))
         
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="room">')
-        self.response.out.write('<optgroup label="Rooms">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.room)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="days">')
-        types = Datastore.OfficeHourJoin.all()
-        self.response.out.write('<optgroup label="Days">')
-        i = 1
-        for oh in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(oh.day)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="faculty_type">')
-        types = Datastore.faculty_type.all()
-        self.response.out.write('<optgroup label="Faculty Type">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.faculty_type)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="degreetype">')
-        types = Datastore.degree_type.all()
-        self.response.out.write('<optgroup label="Degree Type">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.degree_type)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="degreename">')
-        types = Datastore.degree_name.all()
-        self.response.out.write('<optgroup label="Degree Name">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.degree_name)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="institution">')
-        types = Datastore.institution.all()
-        self.response.out.write('<optgroup label="Degree Type">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.institution)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="conference">')
-        types = Datastore.conference_name.all()
-        self.response.out.write('<optgroup label="Conferences">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.conference_name)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="conferencetitle">')
-        types = Datastore.ConferenceJoin.all()
-        self.response.out.write('<optgroup label="Conferences">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.title)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="conferencelocation">')
-        types = Datastore.location.all()
-        self.response.out.write('<optgroup label="Conference Locations">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.location)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="conferenceyear">')
-        types = Datastore.ConferenceJoin.all()
-        self.response.out.write('<optgroup label="Conference Years">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.year)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="journalname">')
-        types = Datastore.journal_name.all()
-        self.response.out.write('<optgroup label="Journals">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.journal_name)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="books">')
-        types = Datastore.BookJoin.all()
-        self.response.out.write('<optgroup label="Books">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.title)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="publisher">')
-        types = Datastore.publisher.all()
-        self.response.out.write('<optgroup label="Book Publishers">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.publisher)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="gradstudents">')
-        types = Datastore.student_eid.all()
-        self.response.out.write('<optgroup label="Graduate Student EID">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.student_eid)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="studenttype">')
-        types = Datastore.student_type.all()
-        self.response.out.write('<optgroup label="Graduate Student Types">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.student_type)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="coursenumber">')
-        types = Datastore.course_number.all()
-        self.response.out.write('<optgroup label="Course Numbers">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.course_number)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-                
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="coursetype">')
-        types = Datastore.course_type.all()
-        self.response.out.write('<optgroup label="Course Types">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.course_type)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')        
-                
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="semeester">')
-        types = Datastore.semester.all()
-        self.response.out.write('<optgroup label="Course Semesters">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.semester)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-                
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="awardtype">')
-        types = Datastore.award_type.all()
-        self.response.out.write('<optgroup label="Award Types">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.award_type)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="institution">')
-        types = Datastore.AwardJoin.all()
-        self.response.out.write('<optgroup label="Award Titles">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.title)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-        
-        self.response.out.write('<select style="margin: 0.2em" multiple="multiple" size="10" name="institution">')
-        types = Datastore.AwardJoin.all()
-        self.response.out.write('<optgroup label="Award Year">')
-        i = 1
-        for fac in types :
-            self.response.out.write('<option value=' + str(i) + '>')
-            self.response.out.write(fac.year)
-            self.response.out.write('</option>')
-            i+=1
-        self.response.out.write('</optgroup></select>')
-
         self.response.out.write('<input type="submit" name="searchaction" value="Search" />')
         
     def post (self) :
         self.response.out.write('<html><body>You wrote:<pre>')
-        self.response.out.write(cgi.escape(self.request.get('name')))
+        for s in searches:
+            self.response.out.write(str(self.request.get(s[0],allow_multiple = True))+'<br>')
         self.response.out.write('</pre></body></html>')
 
